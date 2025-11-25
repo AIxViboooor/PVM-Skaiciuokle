acb5549bc97d1f8aa07e97f118952463e4f4ddec
AIxViboooor <ignas.subs@gmail.com>
2025-11-25 17:04:09 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,11 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9487512376629103"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body>{children}</body>
     </html>
